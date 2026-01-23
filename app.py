ea3cb6aea0e601e481719ffbf6d47cbb1b296fe2
sharifikayvon <sharifikayvon@gmail.com>
2026-01-23 10:40:10 -0500

    initial streamlit app for fitting data

---
+++ app.py
@@ -0,0 +1,224 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import streamlit as st
+from io import BytesIO
+import pandas as pd
+
+
+def sci_notation(x, sig=3):
+    if x == 0:
+        return "0"
+    s = f"{x:.{sig}e}"
+    base, exp = s.split("e")
+    exp = int(exp)
+    return rf"({base}\times 10^{{{exp}}})"
+
+
+def fmt(x, sig=3):
+    return f"{x:.{sig}g}" if ((abs(x) >= 1e-4) & (abs(x) <= 1e4)) else sci_notation(x)
+
+
+st.title("Graph and Fit your Data!")
+
+mode = st.radio(
+    "Choose data input method:",
+    ("Upload data file", "Manually enter data"),
+    horizontal=True
+)
+
+
+if mode == "Upload data file":
+    uploaded_file = st.file_uploader(
+        "Upload a CSV or TSV file",
+        type=["csv", "tsv", "txt"]
+    )
+
+    if uploaded_file is not None:
+        sep = "," if uploaded_file.name.endswith(".csv") else "\t"
+        df = pd.read_csv(uploaded_file, sep=sep)
+
+        st.write("Preview:")
+        st.dataframe(df.head())
+
+        x_col = st.selectbox("X column", df.columns)
+        y_col = st.selectbox("Y column", df.columns)
+
+        xdata = df[x_col].values
+        ydata = df[y_col].values
+
+elif mode == "Manually enter x/y data":
+    st.markdown("Enter values separated by spaces or commas.")
+
+    x_str = st.text_area("X values", "1 2 3 4 5")
+    y_str = st.text_area("Y values", "2 4 1 3 5")
+
+    def parse_array(s):
+        return np.array([float(v) for v in s.replace(",", " ").split()])
+
+    try:
+        xdata = parse_array(x_str)
+        ydata = parse_array(y_str)
+
+        if len(x) != len(y):
+            st.error("X and Y must have the same length.")
+            st.stop()
+
+    except ValueError:
+        st.error("Could not parse numbers. Please check your input.")
+        st.stop()
+
+# uploaded = st.file_uploader("Choose a file", type=["csv", "xlsx", "txt"])
+
+title = st.text_input("Plot Title:", "Title")
+
+col1, col2 = st.columns(2)
+xlabel = col1.text_input("X Label:", "x axis")
+ylabel = col2.text_input("Y Label:", "y axis")
+
+
+col1, col2, col3 = st.columns(3)
+fitline = col1.checkbox("Linear Fit", value=False)
+fitquad = col2.checkbox("Quadratic Fit", value=False)
+darkmode = col3.checkbox("Dark Mode", value=False)
+
+mpl.rcParams.update(
+    {
+            # ===== Figure (outside axes) =====
+            "figure.dpi": 200,
+            "figure.facecolor": "white",  # outside plot
+            "figure.edgecolor": "white",
+            "savefig.dpi": 300,
+            "savefig.format": "png",
+            "savefig.bbox": "tight",
+            "savefig.facecolor": "white",  # saved background
+            "savefig.edgecolor": "white",
+            "figure.autolayout": True,
+            # ===== Axes =====
+            "axes.facecolor": "white",
+            "axes.edgecolor": "black",
+            "axes.linewidth": 1.2,
+            "axes.labelcolor": "black",
+            "axes.labelsize": 20,
+            "axes.titlesize": 20,
+            "axes.titlecolor": "black",
+            "axes.spines.top": True,
+            "axes.spines.right": True,
+            "axes.grid": True,
+            # ===== Grid =====
+            "grid.color": "black",
+            "grid.linewidth": 0.4,
+            "grid.alpha": 0.8,
+            # ===== Ticks =====
+            "xtick.top": True,
+            "ytick.right": True,
+            "xtick.direction": "in",
+            "ytick.direction": "in",
+            "xtick.major.size": 6,
+            "ytick.major.size": 6,
+            "xtick.major.width": 1.2,
+            "ytick.major.width": 1.2,
+            "xtick.minor.visible": True,
+            "ytick.minor.visible": True,
+            "xtick.minor.size": 3,
+            "ytick.minor.size": 3,
+            "xtick.minor.width": 1,
+            "ytick.minor.width": 1,
+            "xtick.color": "black",  # tick marks
+            "ytick.color": "black",
+            "xtick.labelcolor": "black",  # tick labels
+            "ytick.labelcolor": "black",
+            "xtick.labelsize": 16,
+            "ytick.labelsize": 16,
+            "xtick.minor.ndivs": 5,
+            "ytick.minor.ndivs": 5,
+            # ===== Lines & markers =====
+            "lines.linewidth": 1.5,
+            "lines.markersize": 5,
+            "lines.color": "black",
+            # ===== Fonts / math =====
+            "font.family": "serif",
+            "font.serif": ["DejaVu Sans Mono"],
+            "mathtext.default": "regular",
+            # ===== Legend =====
+            "legend.frameon": True,
+            "legend.fontsize": 12,
+            "legend.handlelength": 2,
+            "legend.labelcolor": "black",
+            "legend.facecolor": "white"
+
+    }
+)
+
+c = 'k'
+edgecolors = 'gainsboro'
+
+if darkmode:
+    mpl.rcParams.update(
+        {
+            "figure.facecolor": "black",  # outside plot
+            "figure.edgecolor": "black",
+            "savefig.facecolor": "black",  # saved background
+            "savefig.edgecolor": "black",
+            "axes.facecolor": "black",
+            "axes.edgecolor": "white",
+            "axes.labelcolor": "white",
+            "axes.titlecolor": "white",
+            "grid.color": "snow",
+            "grid.linewidth": 0.4,
+            "grid.alpha": 0.8,
+            "xtick.color": "white",
+            "ytick.color": "white",
+            "xtick.labelcolor": "white",
+            "ytick.labelcolor": "white",
+            "lines.color": "white",
+            "font.family": "serif",
+            "font.serif": ["DejaVu Sans Mono"],
+            "mathtext.default": "regular",
+            "legend.labelcolor": "white",
+            "legend.facecolor": "black"
+        }
+    )
+    c = 'gainsboro'
+    edgecolors = 'w'
+
+if 'xdata' not in locals() and 'ydata' not in locals():
+    xdata = np.arange(0, 4*np.pi, 0.05)
+    ydata = 5 * np.sin(2*xdata) + 1.0 + np.random.normal(0, .5, len(xdata))
+
+fig, ax = plt.subplots(figsize=(14, 8))
+ax.scatter(xdata, ydata, s=100, c=c, edgecolors=edgecolors, lw=3)
+ax.set_xlabel(xlabel)
+ax.set_ylabel(ylabel)
+ax.set_title(title)
+ax.grid(True, which="both")
+
+if fitline:
+    lin_coeffs = np.polyfit(xdata, ydata, 1)
+    lin_xfit = np.linspace(np.min(xdata), np.max(xdata), 500)
+    lin_yfit = np.polyval(lin_coeffs, lin_xfit)
+    lin_label = rf"$Linear\ Fit:\ y\ =\ {fmt(lin_coeffs[0])}x\ +\ {fmt(lin_coeffs[1])}$"
+    ax.plot(lin_xfit, lin_yfit, color="lime", linestyle="--", label=lin_label)
+    ax.legend()
+
+if fitquad:
+    quad_coeffs = np.polyfit(xdata, ydata, 2)
+    quad_xfit = np.linspace(np.min(xdata), np.max(xdata), 500)
+    quad_yfit = np.polyval(quad_coeffs, quad_xfit)
+    quad_label = rf"$Quadratic\ Fit:\ y\ =\ {fmt(quad_coeffs[0])}x^2\ +\ {fmt(quad_coeffs[1])}x\ +\ {fmt(quad_coeffs[2])}$"
+    ax.plot(quad_xfit, quad_yfit, color="b", linestyle="-.", label=quad_label)
+    ax.legend()
+
+
+st.pyplot(fig)
+
+buf = BytesIO()
+fig.savefig(buf, format='png')
+buf.seek(0)
+
+st.download_button(
+    label="Download Graph",
+    data=buf,
+    file_name=f"{title.replace(' ', '_')}.png",
+    mime="image/png"
+)
