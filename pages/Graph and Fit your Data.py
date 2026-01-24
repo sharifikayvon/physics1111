51887098aa017ca3b62b24b1a0880cfe5788ee91
sharifikayvon <sharifikayvon@gmail.com>
2026-01-24 00:15:18 -0500

    Revert "fixed manual entry bugs"
    This reverts commit d1b864b91c31cccb7d220415b362e5f02d340cdc.

--- pages/Graph and Fit your Data.py
+++ pages/Graph and Fit your Data.py
@@ -85,7 +85,7 @@ if mode == "Upload data file":
         xdata = df[x_col].values
         ydata = df[y_col].values
 
-elif mode == "Manually enter data":
+elif mode == "Manually enter x/y data":
     st.markdown("Enter values separated by spaces or commas.")
 
     x_str = st.text_area("X values", "-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8")
@@ -98,7 +98,7 @@ elif mode == "Manually enter data":
         xdata = parse_array(x_str)
         ydata = parse_array(y_str)
 
-        if len(xdata) != len(ydata):
+        if len(x) != len(y):
             st.error("X and Y must have the same length.")
             st.stop()
 
