afa29314ec0484b0ee2660637ee665a8621ffae2
sharifikayvon <sharifikayvon@gmail.com>
2026-01-24 00:36:58 -0500

    changed text_alignment kwarg

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("PHYS 1111 Data Analysis Tools", text_alignment="center")
+st.title("PHYS 1111 Data Analysis Tools", TextAlignment="center")
 
 
 st.markdown("""
