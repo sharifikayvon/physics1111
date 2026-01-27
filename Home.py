c6748675f1cf20c4f3af52bf0b0391a5b75c36fc
sharifikayvon <sharifikayvon@gmail.com>
2026-01-26 22:25:19 -0500

    edited home page

--- Home.py
+++ Home.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="phys1111tools",
+    page_title="phys1111toolkit",
     page_icon="🍎",
     layout="wide"
 )
-st.title("PHYS 1111 Data Analysis Tools", text_alignment="center")
+st.title("phys1111 Data Visualization and Analysis Toolkit", text_alignment="center")
 
 st.markdown("""
-Welcome to the **PHYS 1111 Data Analysis Tools** app! This website was designed to streamline students' daily data visualization and analysis tasks for Dr. Barooni's PHYS 1111 course.
+Welcome to the **phys1111 Data Analysis Toolkit**! This website is designed to 
+streamline routine in-class data visualization and analysis tasks for students in 
+Dr. Barooni's Physics 1111 course and to provide interactive demos of course concepts to aid students' understanding.
+
+Features include:
 - **Graph and Fit your Data**: Make a scatter plot of your data and fit a linear or quadratic function.
-- **Visualize 1D Motion**: Define x(t), v(t), or a(t) and visualize all three!
+- **Visualize 1D Motion**: Define **x(t)**, **v(t)**, or **a(t)** and visualize all three simultaneously.
 
-Explore the tools using the sidebar on the left! If you have any questions or feedback, please email me at ksharifi1@gsu.edu.
+Explore the tools using the sidebar on the left. If you have any questions or feedback, please email me at ksharifi1@gsu.edu.
 
-More features to be added soon. Stay tuned!
+More features to be added soon.
 
 
 """)
