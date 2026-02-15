56039d46453ae238a78c48f30a8ff3ff86d5bc5f
sharifikayvon <sharifikayvon@gmail.com>
2026-02-14 21:37:24 -0500

    foo

--- pages/Graph and Fit your Data 📈.py
+++ pages/Graph and Fit your Data 📈.py
@@ -143,11 +143,11 @@ ylabel = col2.text_input("Y Label:", "y axis")
 
 
 col1, col2, col3, col4, col5 = st.columns(5)
-fitline = col1.checkbox("Linear Fit", value=False)
-fitquad = col2.checkbox("Quadratic Fit", value=False)
-darkmode = col3.checkbox("Dark Mode", value=False)
-flipx = col4.checkbox("Flip x axis", value=False)
-flipy = col5.checkbox("Flip y axis", value=False)
+fitline = col3.checkbox("Linear Fit", value=False)
+fitquad = col4.checkbox("Quadratic Fit", value=False)
+darkmode = col5.checkbox("Dark Mode", value=False)
+flipx = col2.checkbox("Flip x axis", value=False)
+flipy = col1.checkbox("Flip y axis", value=False)
 force_origin = False
 if fitline:
     force_origin = st.checkbox("Set Linear Fit y-intercept to 0", value=False)
