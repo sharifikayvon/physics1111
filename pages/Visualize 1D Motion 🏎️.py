6a90249f4d2c0ca0f945ce367a7f7a17dd5d3037
sharifikayvon <sharifikayvon@gmail.com>
2026-02-14 22:04:14 -0500

    foo

--- pages/Visualize 1D Motion 🏎️.py
+++ pages/Visualize 1D Motion 🏎️.py
@@ -157,7 +157,7 @@ st.markdown(
 )
 
 
-delta_t = st.number_input("final time [s]", value=7.80, format="%0.3f", step=0.001)
+delta_t = st.number_input("Δt (s)", value=7.80, format="%0.3f", step=0.001)
 
 
 t = np.linspace(0, delta_t, int(1e6))
@@ -193,7 +193,7 @@ if usr_func == "x(t)":
 
 elif usr_func == "v(t)":
 
-    x0 = st.number_input("x₀:", value=0.0, step=0.001, format="%0.3f")
+    x0 = st.number_input("x₀ (m)", value=0.0, step=0.001, format="%0.3f")
 
     ufunc, expr, err = parse_function(func_str)
 
@@ -211,7 +211,7 @@ elif usr_func == "v(t)":
 elif usr_func == "a(t)":
 
     x0 = st.number_input("x₀", value=100.0, step=0.001, format="%0.3f")
-    v0 = st.number_input("v₀", value=25.0, step=0.001, format="%0.3f")
+    v0 = st.number_input("v₀ (m/s)", value=25.0, step=0.001, format="%0.3f")
     ufunc, expr, err = parse_function(func_str)
 
     v_expr = sp.integrate(expr, t_sym) + v0
