5bb79e519b9b9b4e76cf5b6e809bb5eaf4809647
sharifikayvon <sharifikayvon@gmail.com>
2026-08-04 12:01:34 -0400

    foo

--- pages/Photos to Spectra 🌈.py
+++ pages/Photos to Spectra 🌈.py
@@ -272,5 +272,5 @@ st.download_button(
 )
 
 st.markdown(
-    "Code to generate the spectrum is adapted from [this repository](https://github.com/nialldeacon/espectrally_for_you)."
+    "Code to generate spectra is adapted from [this repository](https://github.com/nialldeacon/espectrally_for_you)."
 )
